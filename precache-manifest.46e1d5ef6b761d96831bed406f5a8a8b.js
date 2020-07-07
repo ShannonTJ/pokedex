76f5ebf6afb34537902d38222794c68cd4165ef4
Shannon TJ <shannontj@shaw.ca>
2020-07-06 17:45:29 -0700

    Updates

---
+++ precache-manifest.46e1d5ef6b761d96831bed406f5a8a8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15160c91c946eb153f2007dd3a246c45",
+    "url": "/simple-dex/index.html"
+  },
+  {
+    "revision": "17085250e2ebd11c9463",
+    "url": "/simple-dex/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "52dd88bdb71a0d583044",
+    "url": "/simple-dex/static/css/main.460b89dc.chunk.css"
+  },
+  {
+    "revision": "17085250e2ebd11c9463",
+    "url": "/simple-dex/static/js/2.48398a97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-dex/static/js/2.48398a97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52dd88bdb71a0d583044",
+    "url": "/simple-dex/static/js/main.51aba980.chunk.js"
+  },
+  {
+    "revision": "1cea9618297a3028dba8",
+    "url": "/simple-dex/static/js/runtime-main.d053d478.js"
+  }
+]);
